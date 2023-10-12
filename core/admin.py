13188e8eb58a4f63e7896346383f647ac510ed38
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2023-10-12 15:47:51 +0600

    12.10.2023

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,62 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from core.models import Tag, Category, Product, ProductImage, ProductAttribute
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+class ProductAdminForm(forms.ModelForm):
+
+    content = forms.CharField(widget=CKEditorUploadingWidget(), label='Контент')
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductImageStackedInline(admin.StackedInline):
+
+    model = ProductImage
+    extra = 1
+
+
+class ProductAttributeStackedInline(admin.StackedInline):
+
+    model = ProductAttribute
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'is_published', 'get_image')
+    list_display_links = ('id', 'name',)
+    list_filter = ('category', 'tags', 'user', 'is_published',)
+    search_fields = ('name', 'description', 'content',)
+    readonly_fields = ('created_at', 'updated_at', 'get_big_image',)
+    form = ProductAdminForm
+    inlines = [ProductAttributeStackedInline, ProductImageStackedInline]
+
+    @admin.display(description='Изображение')
+    def get_image(self, item):
+        return mark_safe(f'<img src="{item.image.url}" width="150px">')
+
+    @admin.display(description='Изображение')
+    def get_big_image(self, item):
+        return mark_safe(f'<img src="{item.image.url}" width="100%">')
 
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django_resized import ResizedImageField
+
+
+class TimeStampAbstractModel(models.Model):
+    created_at = models.DateTimeField('дата добавление', auto_now_add=True)
+    updated_at = models.DateTimeField('дата изменения', auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimeStampAbstractModel):
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    name = models.CharField('название', max_length=250, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Tag(TimeStampAbstractModel):
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+
+    name = models.CharField('название', max_length=255)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(TimeStampAbstractModel):
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('-created_at',)
+
+    name = models.CharField('название', max_length=100)
+    description = models.CharField('описание', max_length=255)
+    content = models.TextField('контент')
+    category = models.ForeignKey('core.Category', models.PROTECT, verbose_name='категория')
+    tags = models.ManyToManyField('core.Tag', verbose_name='теги')
+    price = models.DecimalField('цена', max_digits=6, decimal_places=2, default=0.0)
+    user = models.ForeignKey('auth.User', models.CASCADE, verbose_name='пользователь')
+    is_published = models.BooleanField('публичность', default=True)
+
+    @property
+    def image(self):
+        return self.images.first().image
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class ProductImage(TimeStampAbstractModel):
+    class Meta:
+        verbose_name = 'изображение товара'
+        verbose_name_plural = 'изображении товаров'
+        ordering = ('-created_at',)
+
+    product = models.ForeignKey('core.Product', models.CASCADE, 'images', verbose_name='товар')
+    image = ResizedImageField('изображение', upload_to='product_images/', force_format='WEBP', quality=90)
+
+    def __str__(self):
+        return f'{self.product.name}'
+
+
+class ProductAttribute(TimeStampAbstractModel):
+
+    class Meta:
+        verbose_name = 'атрибут товара'
+        verbose_name_plural = 'атрибуты товаров'
+        ordering = ('-created_at',)
+
+    name = models.CharField('название', max_length=50)
+    value = models.CharField('значение', max_length=50)
+    product = models.ForeignKey('core.Product', models.CASCADE, 'attributes', verbose_name='товар')
+
+    def __str__(self):
+        return f'{self.name} - {self.value}'
 
 # Create your models here.
